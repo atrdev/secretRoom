18b31f51c82a55b0b87555491d63f9a5bb4d7e9c
atrdev <arthurthomazette@hotmail.com>
2022-05-31 23:23:44 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
-window.onmessage = (event) => {
+/*window.onmessage = (event) => {
    console.log(event);
     if (event.data) {
        let receivedData = event.data;
        console.log(event.data);
     }
-  };
+  };*/
 var myFunction = function(){
     console.log("myFunction")
 }
