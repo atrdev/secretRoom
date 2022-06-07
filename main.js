4773ede32b3118256d69d67ff31ff583ae7e3e32
atrdev <arthurthomazette@hotmail.com>
2022-06-07 19:55:02 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
-/*window.onmessage = (event) => {
-   console.log(event);
-    if (event.data) {
-       let receivedData = event.data;
-       console.log(event.data);
-    }
-  };*/
-var myFunction = function(){
-    console.log("myFunction")
+window.onload = function(){
+   try_get_frame();
 }
-console.log("TESTEEEEEEE")
+const try_get_frame = function(){
+   let _frames = document.querySelectorAll('iframe');
+   if(frame.length ==0){
+      setTimeout(try_get_frame,1000);
+   }else{
+      _frames.forEach((_frame)=>{
+         var cssLink = document.createElement("link");
+         cssLink.href = "https://cdn.jsdelivr.net/gh/atrdev/secretRoom/main.css"; 
+         cssLink.rel = "stylesheet"; 
+         cssLink.type = "text/css"; 
+         _frame.document.head.appendChild(cssLink);
+      });
+   }
+   
+}
