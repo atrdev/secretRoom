0fa08cecb3512cf48306df7e77585199d77bbe08
atrdev <arthurthomazette@hotmail.com>
2022-06-07 19:56:55 -0300

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,13 @@ window.onload = function(){
 }
 const try_get_frame = function(){
    let _frames = document.querySelectorAll('iframe');
+   console.log("Try get");
+   
    if(frame.length ==0){
       setTimeout(try_get_frame,1000);
    }else{
+      console.log("Inject");
+   
       _frames.forEach((_frame)=>{
          var cssLink = document.createElement("link");
          cssLink.href = "https://cdn.jsdelivr.net/gh/atrdev/secretRoom/main.css"; 
